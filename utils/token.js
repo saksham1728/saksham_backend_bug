3eb477a517da5bebcf5395e0ead30c585c31377e
saksham1728 <148770428+saksham1728@users.noreply.github.com>
2024-08-03 17:22:07 +0530

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'; // Import jwt using ES module syntax
 
 export const createNewToken = (payload) => {
-    return jwt.sign({ userId: payload }, process.env.JWT_SECRET_KEY, { expiresIn: '10d' }); // Correctly use 'process.env.JWT_SECRET_KEY'
+    return jwt.sign({ userId: payload }, process.env.SECRET_KEY, { expiresIn: '10d' }); // Correctly use 'process.env.JWT_SECRET_KEY'
 };
